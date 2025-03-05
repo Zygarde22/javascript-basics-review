9beada5039877e524ff79cd3977569f7421e88ae
Khris R <khristonrobinson@gmail.com>
2025-03-04 19:20:38 -0600

    added code and other variables

---
+++ variables.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// Using var
+var a = 23;
+var b = 33;
+var c = a + b;
+console.log(c);  
+
+// Using let
+let d = 23;
+let e = 33;
+let f = d + e;
+console.log(f);  
+// Using const
+const g = 99;
+const h = 40;
+const i = g - h;
+console.log(i);  
+
+
+//Strings//
+let myString= "Daisybell";
+console.log(myString);
+console.log(typeof myString);
+
+//number//
+let myNumber = 42;
+let myFloat = 3.14;
+console.log(myNumber, myFloat); 
+console.log(typeof myNumber); 
+
+//boolean//
+let isCodingFun = true;
+let isBoring = false;
+console.log(isCodingFun, isBoring);  
+console.log(typeof isCodingFun);  
+
+//objects//
+
+let myObject = {
+    name: "Khriston",
+    age: 29,
+    isBirthday: false  
+};
+
+console.log(myObject);
+
+//null//
+let myNullValue = null;
+console.log(myNullValue); 
+console.log(typeof myNullValue);
+
