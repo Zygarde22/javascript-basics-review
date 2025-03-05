bddefcbdbdc0e87adfaf09d75127cb2c288e9aaa
Khris R <khristonrobinson@gmail.com>
2025-03-04 19:33:40 -0600

    added objects.js and filled out code

---
+++ objects.js
@@ -0,0 +1,22 @@
+"use strict";
+
+"use strict";
+
+// Defining an object with properties
+let person = {
+    name: "Khriston",
+    age: 29,
+    isStudent: false
+};
+
+ 
+console.log(person.name);    
+console.log(person.age);     
+console.log(person.isStudent); 
+
+ 
+person.age = 30;  
+person.isStudent = true; 
+
+console.log(person.age);     
+console.log(person.isStudent);  
